e2bf1a7236375e123600284980d281d4e9d043a7
evaristofm <evaristofm@gmail.com>
2024-06-06 12:16:03 -0300

    Adicionando tratamento de erros, logs e arquivos crm e erp data.json

--- order-integration/handler.py
+++ order-integration/handler.py
@@ -1,11 +1,15 @@
 import re
 import json
 import boto3
+import logging
 from datetime import datetime, date
 
 
+S3_BUCKET = '{seu-bucket-name}'
+
 s3_client = boto3.client("s3")
-S3_BUCKET = 'seu-bucket-name'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def transforma_dados(dados):
     pedido_final = []
@@ -30,43 +34,65 @@ def json_serial(obj):
 
 def erp_handler(event, context):
 
-    body = event.get('body')
-    body_pedidos_dict = json.loads(body)
+    try:
+        body = event.get('body')
+        body_pedidos_dict = json.loads(body)
 
-    pedidos = transforma_dados(body_pedidos_dict)
-    #TODO: Pesquisar o erro de runtime ao utilizar a dunção map para transformar os dados. https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html
+        pedidos = transforma_dados(body_pedidos_dict)
+        #TODO: Pesquisar o erro de runtime ao utilizar a dunção map para transformar os dados. https://docs.aws.amazon.com/lambda/latest/dg/runtimes-update.html
 
-    filename = 'new_erp_data' + '.json'
-    uploadByteStream = bytes(json.dumps(
-        pedidos,
-        indent=4,
-        default=json_serial).encode('UTF-8'))
+        filename = 'new_erp_data' + '.json'
+        uploadByteStream = bytes(json.dumps(
+            pedidos,
+            indent=4,
+            default=json_serial).encode('UTF-8'))
 
-    s3_client.put_object(Bucket=S3_BUCKET, Key=filename, Body=uploadByteStream)
+        s3_client.put_object(Bucket=S3_BUCKET, Key=filename, Body=uploadByteStream)
 
-    print(pedidos)
+        print(pedidos)
 
-    return {'statusCode': 200}
+        return {
+             "statusCode": 200,
+             "body": json.dumps("Dados processados e armazenados com sucesso no S3")
+            }
 
+    except Exception as e:
+        logger.error(f"Error ao processar ERP data: {str(e)}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps("Internal Server Error")
+        }
+    
 
 def crm_handler(event, context):
     """Ler arquivo com os dados transformados no s3"""
+    try:
+         
+        object_key = 'new_erp_data' + '.json'
+
+        file_content = json.loads(s3_client.get_object(
+            Bucket=S3_BUCKET, Key=object_key)['Body'].read())
+        
+        print(file_content)
+
+        """Envio dos dados para s3 com o arquivo crm_swagger.json"""
+        filename = 'crm_swagger' + '.json'
+
+        uploadByteStream = bytes(json.dumps(
+            file_content,
+            indent=4,
+            default=json_serial).encode('UTF-8'))
+
+        s3_client.put_object(Bucket=S3_BUCKET, Key=filename, Body=uploadByteStream)
+
+        return {
+            "statusCode": 200,
+             "body": json.dumps("Dados enviados com sucesso para CRM")
+        }
     
-    object_key = 'new_erp_data' + '.json'
-
-    file_content = json.loads(s3_client.get_object(
-        Bucket=S3_BUCKET, Key=object_key)['Body'].read())
-    
-    print(file_content)
-
-    """Envio dos dados para s3 com o arquivo crm_swagger.json"""
-    filename = 'crm_swagger' + '.json'
-
-    uploadByteStream = bytes(json.dumps(
-        file_content,
-        indent=4,
-        default=str).encode('UTF-8'))
-
-    s3_client.put_object(Bucket=S3_BUCKET, Key=filename, Body=uploadByteStream)
-
-    return {'statusCode': 200}
+    except Exception as e:
+        logger.error(f"Error ao processar CRM data: {str(e)}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps("Internal Server Error")
+        }
